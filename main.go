d416934faddb4515191d313f5ac85d3e0d45eb75
Piotr Ostrowski <piotrostr@google.com>
2022-11-01 13:20:55 +0000

    for loop with goroutines example

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"io"
+	"log"
+	"os"
+	"sync"
 )
 
+func process(val string) {}
+
+var logger = log.New(os.Stdout, "", log.LstdFlags)
+
 func main() {
-	sth := (*io.Writer)(nil)
-	fmt.Println(sth)
+	values := []string{"a", "b", "c"}
+	wg := sync.WaitGroup{}
+	wg.Add(len(values))
+	for _, val := range values {
+		go func(val string) {
+			defer wg.Done()
+			logger.Printf("Processing %s...", val)
+			process(val)
+		}(val)
+	}
+	wg.Wait()
+	logger.Println("Done")
 }
